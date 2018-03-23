281e359e6b80b5b1d0b3d1aeb9e3c4c65f0f0a7a
Dmytro Kabachenko <Dmytro_Kabachenko@epam.com>
2018-03-23 12:54:31 +0200

    change color

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h2 style='color:green'>Hello World!</h2>\n"
+  return "<h2 style='color:black'>Hello World!</h2>\n"
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
